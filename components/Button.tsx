114c1d4bd5bda1f9ab565b53414c49baa3c25107
MicoFeb <sutanmico@gmail.com>
2023-06-02 17:24:35 +0700

    Layout Done

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, children, disabled, type = 'Button', ...props }, ref) => {
+	({ className, children, disabled, type = 'button', ...props }, ref) => {
 		return (
 			<button
 				type={type}
